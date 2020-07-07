c063d3bd6d20f2cc10561c94fdbfb1a2e2245da9
Abel Ponce <vnponce8@gmail.com>
2020-07-07 11:43:30 -0500

    select char

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import { Planet, Backpack, Browser, Cat, Chocolate, CreditCard, File, Ghost, IceCream, Mug, SpeechBubble } from 'react-kawaii';
+import styled from 'styled-components';
 import './App.css';
 
+const SelectCharacterStyled = styled.div`
+  height: 50%;
+  width: 60%;
+  display: flex;
+  .box {
+    border: 1px solid red;
+  }
+  .preview {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 40%;
+  }
+  .options {
+    width: 60%;
+    display: flex;
+    flex-wrap: wrap;
+    span {
+      height: fit-content;
+      background: cadetblue;
+      color: white;
+      padding: 1em;
+      margin: 1em;
+      border-radius: 5px;
+      cursor: pointer;
+      &:hover {
+        background: darkcyan;
+      }
+    }
+  }
+`;
+
 function App() {
+  const [CurrentCharacter,setCurrentCharacter] = useState('Planet');
+  const components = {
+    Backpack,
+    Browser,
+    Cat,
+    Chocolate,
+    CreditCard,
+    File,
+    Ghost,
+    IceCream,
+    Mug,
+    Planet,
+    SpeechBubble,
+  }
+
+  function changeCharacter(character) {
+    setCurrentCharacter(character);
+  }
+  console.log('char =>', components[CurrentCharacter]);
+  const Char = components[CurrentCharacter];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SelectCharacterStyled>
+        <div className="box preview">
+          <Char size={200} mood="blissful" color="#FDA7DC" />
+        </div>
+        <div className="box options">
+          {Object.keys(components).map(item => <span onClick={() => changeCharacter(item)}>{item}</span>)}
+        </div>
+      </SelectCharacterStyled>
     </div>
   );
 }
