74cd720dca3444e0be83a9f4a2a955d11816a80c
Abel Ponce <vnponce8@gmail.com>
2020-07-09 22:40:04 -0500

    ghost without framer motion

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -7,36 +7,6 @@ import { motion } from "framer-motion"
 const HeroStyled = styled.div`
     width: 100%;
     height: 100vh;
-    header {
-      color: var(--baseColor);
-      width: 100%;
-      height: 100px;
-      display: flex;
-      align-items: center;
-      justify-content: flex-end;
-      padding: 30px;
-      box-sizing: border-box;
-      background: var(--basebackGround);
-      border-bottom: 3px solid var(--baseColor);
-      a {
-        margin-left: 30px;
-        text-decoration: none;
-        color: inherit;
-      }
-      label, [type=checkbox] {
-        cursor:pointer;
-      }
-    }
-    main {
-      background: var(--basebackGround);
-      display: flex;
-      width: 100%;
-      height: calc(100% - 100px);
-      align-items: center;
-      justify-content: center;
-      flex-direction: column;
-      color: var(--baseColor)
-    }
 `;
 
 function Hero() {
@@ -84,14 +54,10 @@ function Hero() {
        </label>
      </header>
      <main className="hero-text">
-       {/*Motion viene de la librería framer-motion https://www.framer.com/motion/ para realizar animaciones*/}
-       <motion.div
-         initial={{ opacity: 0.5 }}
-         animate={{ y: 15, x: 10, opacity: 1 }}
-         transition={{ yoyo: Infinity, duration: 3, ease: "easeInOut" }}
-       >
+       <div id="ghost">
          <Ghost size={200} mood="blissful" {...ghost} />
-       </motion.div>
+       </div>
+       <div id="shadow"></div>
      </main>
    </HeroStyled>
  );
