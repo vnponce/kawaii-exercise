7ade4c39b127a5d889006c70923c54c5a7b691b1
Abel Ponce <vnponce8@gmail.com>
2020-07-09 23:07:36 -0500

    Updates

---
+++ precache-manifest.9d95aa4280b94bac2aa023d05c608319.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb3997c9b3c86abf211ae03e4de6cc31",
+    "url": "/kawaii-exercise/index.html"
+  },
+  {
+    "revision": "1ff4b00b0a51fa8b117d",
+    "url": "/kawaii-exercise/static/css/main.e5f35acc.chunk.css"
+  },
+  {
+    "revision": "df2f6bf74fe63d8bf456",
+    "url": "/kawaii-exercise/static/js/2.9b2717f2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kawaii-exercise/static/js/2.9b2717f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ff4b00b0a51fa8b117d",
+    "url": "/kawaii-exercise/static/js/main.2b55885e.chunk.js"
+  },
+  {
+    "revision": "8372ccd9d82bf10d0e6c",
+    "url": "/kawaii-exercise/static/js/runtime-main.b1225a6b.js"
+  }
+]);
